3f614119e7b74957a4048bd1a3537c1a495c0db4
afolabiopakunle <opakunleafolabi@gmail.com>
2021-12-23 14:52:26 +0100

    basic highlighter implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, BasicHighlightDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
